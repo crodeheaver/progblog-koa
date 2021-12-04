e84f3df9ebd22f05672e45e85f3f3a4372d9f50c
Colby Rodeheaver <colbyrodeheaver@hotmail.com>
2021-12-04 17:16:16 -0600

    add some comments to main.js

--- main.js
+++ main.js
@@ -8,19 +8,26 @@ import path from 'path';
 const __dirname = path.resolve();
 const app = new koa();
 
+//add security headers
 app.use(helmet());
 
+//server static content from public folder
 app.use(serve(__dirname + '/public/'))
 
+//add handlebars template support
 app.use(handlebars({
   defaultLayout: "main"
 }));
 
+//add middleware to access db
 app.use(db);
+
+//add routes
 app.use(router);
 
+//get and print port
 const port = process.env.PORT || 8080;
-
 console.log(`Process running on port: ${port}`)
 
+//run application
 app.listen(port);
