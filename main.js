1b9a83697c14a93154d697ea26d49468154d5b76
Colby Rodeheaver <colbyrodeheaver@hotmail.com>
2021-12-04 18:05:16 -0600

    moved to koa-views

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 import koa from 'koa'
 import serve from 'koa-static'
-import handlebars from 'koa-handlebars'
 import helmet from 'koa-helmet';
+import views from 'koa-views'
 import router from './routes/index.js'
 import db from './middleware/knex-middleware.js'
 import path from 'path';
+
 const __dirname = path.resolve();
 const app = new koa();
+
+const render = views(__dirname + '/views', {
+  map: { hbs: 'handlebars' },
+    options: {
+      partials: {
+        mainLayout: './partials/main-layout' // requires ./my-partial.hbs
+      }
+    }
+})
+
+console.log(render)
 
 //add security headers
 app.use(helmet());
@@ -15,9 +27,7 @@ app.use(helmet());
 app.use(serve(__dirname + '/public/'))
 
 //add handlebars template support
-app.use(handlebars({
-  defaultLayout: "main"
-}));
+app.use(render);
 
 //add middleware to access db
 app.use(db);
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', async function (ctx) {
 
     const posts = await ctx.db.posts.get()
 
-    await ctx.render("index", {
+    await ctx.render("index.hbs", {
       title: "Progblog - Home",
       posts: posts.toJSON()
     });
