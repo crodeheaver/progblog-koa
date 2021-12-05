d1d6b30d433f574302fdd905c28400d0a7115a9a
Colby Rodeheaver <colbyrodeheaver@hotmail.com>
2021-12-04 21:53:34 -0600

    update schema creation

--- database/schema.js
+++ database/schema.js
@@ -1,25 +1,43 @@
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-        host: 'localhost',
-        user: 'root',
-        password: 'my-secret-pw',
-        database: 'progblog',
-        charset: 'utf8',
-        insecureAuth: true
-    }
-})
+import knexfactory from 'knex'
 
+const connectionOptions = {
+    host: 'localhost',
+    user: 'root',
+    password: 'my-secret-pw',
+    charset: 'utf8',
+    insecureAuth: true
+}
 
-knex.schema.createTable('post', function (table) {
-    table.uuid('id')
-    table.primary('id')
-    table.string('title', 100)
-    table.text('body')
-    table.string('slug', 30)
-    table.timestamps(false, true)
-}).then(() => console.log("post table created"))
-    .catch((err) => { console.log(err); throw err })
-    .finally(() => {
-        knex.destroy();
-    });
+let knex = knexfactory({ client: 'mysql', connection: connectionOptions })
+
+try {
+    await knex.raw('CREATE DATABASE progblog')
+}
+catch(err){
+    console.log('failed to create database')
+}
+finally {
+    knex.destroy()
+}
+
+// connect with database selected
+connectionOptions.database = 'progblog';
+knex = knexfactory({ client: 'mysql', connection: connectionOptions });
+
+try {
+    await knex.schema.createTable('post', function (table) {
+        table.uuid('id')
+        table.primary('id')
+        table.string('title', 100)
+        table.text('body')
+        table.string('slug', 30)
+        table.timestamps(false, true)
+    })
+    console.log("post table created")
+}
+catch(err){
+    console.log('failed to create post table')
+}
+finally {
+    knex.destroy();
+}
